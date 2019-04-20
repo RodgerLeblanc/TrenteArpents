8f45c28e8e0eee4af51ca755f54f2ac517acb349
Roger Leblanc <roger@rogerleblanc.net>
2019-04-19 20:06:25 -0400

    Initial commit

---
+++ TrenteArpents/TrenteArpents/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using TrenteArpents.Models;
+
+namespace TrenteArpents.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
