dedb86c3d017bf49bf69f87126e32bc2c2269c2c
Roger Leblanc <roger@rogerleblanc.net>
2019-04-26 23:17:44 -0400

    Change photos page for Multimedia page

---
+++ TrenteArpents/TrenteArpents/ViewModels/MultimediaViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace TrenteArpents.ViewModels
+{
+    public class MultimediaViewModel : BaseViewModel
+    {
+        private const string facebookPhotosUrl =
+            "https://www.facebook.com/pg/Comit%C3%A9-des-r%C3%A9sidents-du-Domaine-des-30-Arpents-107465872723807/photos/?tab=album&album_id=1377116752425373";
+
+        private const string youtubeVideoUrl =
+            "https://www.youtube.com/watch?v=PZRo_xz50sI";
+
+        public MultimediaViewModel()
+        {
+            OpenFacebookPhotosCommand = new Command(() => Device.OpenUri(new Uri(facebookPhotosUrl)));
+            OpenVideoCommand = new Command(() => Device.OpenUri(new Uri(youtubeVideoUrl)));
+        }
+
+        public ICommand OpenFacebookPhotosCommand { get; }
+        public ICommand OpenVideoCommand { get; }
+    }
+}
--- TrenteArpents/TrenteArpents/Views/Multimedia.xaml.cs
+++ TrenteArpents/TrenteArpents/Views/Multimedia.xaml.cs
@@ -3,21 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TrenteArpents.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace TrenteArpents.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Photos : ContentPage
+    public partial class Multimedia : ContentPage
     {
-        public Photos()
+        public Multimedia()
         {
             InitializeComponent();
-
-            webView.Source = 
-                "https://www.facebook.com/pg/Comit%C3%A9-des-r%C3%A9sidents-du-Domaine-des-30-Arpents-107465872723807/photos/?tab=album&album_id=1377116752425373";
+            BindingContext = new MultimediaViewModel();
         }
     }
 }
