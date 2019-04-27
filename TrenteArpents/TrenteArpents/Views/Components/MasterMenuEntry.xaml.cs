5ad6ed58f474ea547b93dda8dc3ebfc9538633ae
Roger Leblanc <roger@rogerleblanc.net>
2019-04-27 16:35:16 -0400

    Change Master menu from ListView to ScrollView

---
+++ TrenteArpents/TrenteArpents/Views/Components/MasterMenuEntry.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TrenteArpents.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MasterMenuEntry : ContentView
+    {
+        Page pageCache;
+
+        public MasterMenuEntry()
+        {
+            InitializeComponent();
+            AddTapHandler();
+        }
+
+        private void AddTapHandler()
+        {
+            GestureRecognizers.Add(GetTapGestureRecognizer());
+        }
+
+        private TapGestureRecognizer GetTapGestureRecognizer()
+        {
+            TapGestureRecognizer tapGestureRecognizer = new TapGestureRecognizer();
+            tapGestureRecognizer.Tapped += OnTapped;
+
+            return tapGestureRecognizer;
+        }
+
+        private void OnTapped(object sender, EventArgs e)
+        {
+            if (MasterDetailPage.Detail.GetType() == PageType)
+            {
+                MasterDetailPage.IsPresented = false;
+                return;
+            }
+
+            if (pageCache == null)
+            {
+                pageCache = GetPage() ?? throw new InvalidOperationException("Couldn't create a Page.");
+            }
+
+            MasterDetailPage.Detail = pageCache;
+            MasterDetailPage.IsPresented = false;
+        }
+
+        private Page GetPage()
+        {
+            if (PageType == null)
+            {
+                throw new InvalidOperationException("PageType is null.");
+            }
+
+            Page page = (Page)Activator.CreateInstance(PageType);
+            return new NavigationPage(page);
+        }
+
+        private MasterDetailPage MasterDetailPage
+        {
+            get
+            {
+                return Application.Current.MainPage as MasterDetailPage;
+            }
+        }
+
+        public static readonly BindableProperty ImageSourceProperty =
+            BindableProperty.Create(nameof(ImageSource), typeof(ImageSource), typeof(MasterMenuEntry), null);
+
+        public static readonly BindableProperty TitleProperty =
+            BindableProperty.Create(nameof(Title), typeof(string), typeof(MasterMenuEntry), null);
+
+        public static readonly BindableProperty PageTypeProperty =
+            BindableProperty.Create(nameof(PageType), typeof(Type), typeof(MasterMenuEntry), null, propertyChanged: OnPageTypeChanged);
+
+        public ImageSource ImageSource
+        {
+            get { return (ImageSource)GetValue(ImageSourceProperty); }
+            set { SetValue(ImageSourceProperty, value); }
+        }
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public Type PageType
+        {
+            get { return (Type)GetValue(PageTypeProperty); }
+            set { SetValue(PageTypeProperty, value); }
+        }
+
+        private static void OnPageTypeChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (bindable is MasterMenuEntry entry)
+            {
+                entry.pageCache = null;
+            }
+        }
+    }
+}
--- TrenteArpents/TrenteArpents/Views/MainMenuMaster.xaml.cs
+++ TrenteArpents/TrenteArpents/Views/MainMenuMaster.xaml.cs
@@ -9,74 +9,10 @@ namespace TrenteArpents.Views
 {
     public partial class MainMenuMaster : ContentPage
     {
-        Dictionary<Type, Page> pageCache = new Dictionary<Type, Page>();
-
         public MainMenuMaster()
         {
             InitializeComponent();
             BindingContext = new MainMenuMasterViewModel();
-        }
-
-        private MasterDetailPage MasterDetailPage
-        {
-            get 
-            {
-                if (Parent is MasterDetailPage masterDetailPage)
-                {
-                    return masterDetailPage;
-                }
-
-                return Application.Current.MainPage as MasterDetailPage;
-            }
-        }
-
-        private void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
-        {
-            if (!(e.SelectedItem is DetailPageInfo detailPageInfo))
-                return;
-
-            RemoveSelection();
-            SetDetailPage(detailPageInfo);
-            CloseMasterPage();
-        }
-
-        private void RemoveSelection()
-        {
-            listView.SelectedItem = null;
-        }
-
-        private void SetDetailPage(DetailPageInfo detailPageInfo)
-        {
-            if (MasterDetailPage.Detail.GetType() == detailPageInfo.PageType)
-            {
-                return;
-            }
-
-            MasterDetailPage.Detail = GetDetailPage(detailPageInfo.PageType);
-        }
-
-        private Page GetDetailPage(Type pageType)
-        {
-            if (pageCache.ContainsKey(pageType))
-            {
-                return pageCache[pageType];
-            }
-
-            Page page = (Page)Activator.CreateInstance(pageType);
-            NavigationPage navigationPage = new NavigationPage(page);
-
-            AddPageToCache(pageType, navigationPage);
-            return navigationPage;
-        }
-
-        private void AddPageToCache(Type pageType, NavigationPage navigationPage)
-        {
-            pageCache.Add(pageType, navigationPage);
-        }
-
-        private void CloseMasterPage()
-        {
-            MasterDetailPage.IsPresented = false;
         }
     }
 }
