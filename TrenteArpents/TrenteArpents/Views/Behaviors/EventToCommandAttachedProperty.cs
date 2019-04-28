410aea0fa069096c19dbd6e92a69a917f3e45b21
Roger Leblanc <roger@rogerleblanc.net>
2019-04-28 03:09:27 -0400

    Add EventToCommandBehavior

---
+++ TrenteArpents/TrenteArpents/Views/Behaviors/EventToCommandAttachedProperty.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TrenteArpents.ViewModels;
+using Xamarin.Forms;
+
+namespace TrenteArpents.Views
+{
+    public partial class EventToCommandBehavior
+    {
+        public static bool GetAttachAppearing(BindableObject obj)
+        {
+            return (bool)obj.GetValue(AttachAppearingProperty);
+        }
+
+        public static void SetAttachAppearing(BindableObject obj, bool value)
+        {
+            obj.SetValue(AttachAppearingProperty, value);
+        }
+
+        public static readonly BindableProperty AttachAppearingProperty =
+            BindableProperty.CreateAttached(
+                "AttachAppearing", 
+                typeof(bool), 
+                typeof(EventToCommandBehavior), 
+                defaultValue: false, 
+                propertyChanged: OnAttachAppearingChanged);
+
+        private static void OnAttachAppearingChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (!(bindable is VisualElement view))
+            {
+                return;
+            }
+
+            if (view.Behaviors.OfType<EventToCommandBehavior>().Any(b => b.EventName == nameof(ContentPage.Appearing)))
+            {
+                return;
+            }
+
+            view.Behaviors.Add(CreateAppearingEventToCommandBehavior(view));
+        }
+
+        private static Behavior CreateAppearingEventToCommandBehavior(VisualElement view)
+        {
+            var behavior = new EventToCommandBehavior { EventName = nameof(ContentPage.Appearing) };
+
+            view.BindingContextChanged += (s, e) =>
+            {
+                behavior.SetBinding(CommandProperty, new Binding(nameof(BaseViewModel.AppearingCommand))
+                {
+                    Source = view.BindingContext
+                });
+            };
+
+            return behavior;
+        }
+    }
+}
---
+++ TrenteArpents/TrenteArpents/Views/Behaviors/EventToCommandBehavior.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace TrenteArpents.Views
+{
+    public partial class EventToCommandBehavior : Behavior<Element>
+    {
+        private EventInfo eventInfo;
+        private Delegate eventHandler;
+        private ICommand internalCommand;
+
+        public Element View { get; private set; }
+
+        public static readonly BindableProperty EventNameProperty =
+          BindableProperty.Create(nameof(EventName), typeof(string), typeof(EventToCommandBehavior), null, propertyChanged: OnEventNameChanged);
+
+        public static readonly BindableProperty CommandProperty =
+          BindableProperty.Create(nameof(Command), typeof(ICommand), typeof(EventToCommandBehavior), null, propertyChanged: OnCommandChanged);
+
+        private static void OnCommandChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (bindable is EventToCommandBehavior behavior)
+            {
+                if (behavior.internalCommand != newValue)
+                {
+                    behavior.internalCommand = (ICommand)newValue;
+                }
+            }
+        }
+
+        public static readonly BindableProperty CommandParameterProperty =
+          BindableProperty.Create(nameof(CommandParameter), typeof(object), typeof(EventToCommandBehavior), null);
+
+        public static readonly BindableProperty InputConverterProperty =
+          BindableProperty.Create(nameof(Converter), typeof(IValueConverter), typeof(EventToCommandBehavior), null);
+
+        public string EventName { get; set; }
+        public ICommand Command { get; set; }
+        public object CommandParameter { get; set; }
+        public IValueConverter Converter { get; set; }
+
+        protected override void OnAttachedTo(Element bindable)
+        {
+            base.OnAttachedTo(bindable);
+
+            View = bindable;
+
+            RegisterEvent(EventName);
+        }
+
+        protected override void OnDetachingFrom(Element bindable)
+        {
+            DeregisterEvent();
+
+            base.OnDetachingFrom(bindable);
+        }
+
+        void RegisterEvent(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                return;
+            }
+
+            eventInfo = View.GetType().GetRuntimeEvent(name);
+
+            if (eventInfo == null)
+            {
+                throw new ArgumentException($"Can't register the {name} event.");
+            }
+
+            MethodInfo methodInfo = GetMethod(nameof(OnEvent));
+            eventHandler = methodInfo.CreateDelegate(eventInfo.EventHandlerType, this);
+            eventInfo.AddEventHandler(View, eventHandler);
+        }
+
+        private MethodInfo GetMethod(string methodName)
+        {
+            return typeof(EventToCommandBehavior).GetTypeInfo().GetDeclaredMethod(methodName);
+        }
+
+        void OnEvent(object sender, object eventArgs)
+        {
+            if (internalCommand == null)
+            {
+                return;
+            }
+
+            object resolvedParameter = GetParameter(eventArgs);
+
+            if (internalCommand.CanExecute(resolvedParameter))
+            {
+                internalCommand.Execute(resolvedParameter);
+            }
+        }
+
+        private object GetParameter(object eventArgs)
+        {
+            if (CommandParameter != null)
+            {
+                return CommandParameter;
+            }
+
+            if (Converter != null)
+            {
+                return Converter.Convert(eventArgs, typeof(object), null, null);
+            }
+
+            return eventArgs;
+        }
+
+        private static void OnEventNameChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (bindable is EventToCommandBehavior behavior)
+            {
+                behavior.DeregisterEvent();
+                behavior.RegisterEvent((string)newValue);
+            }
+        }
+
+        private void DeregisterEvent()
+        {
+            eventInfo?.RemoveEventHandler(View, eventHandler);
+        }
+    }
+}
