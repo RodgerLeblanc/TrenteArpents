330daa612cd015428ec202a0aed7d1dbef316b00
Roger Leblanc <roger@rogerleblanc.net>
2019-05-05 09:13:19 -0400

    Enhance Android splashscreen

--- TrenteArpents/TrenteArpents.Android/MainActivity.cs
+++ TrenteArpents/TrenteArpents.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace TrenteArpents.Droid
 {
-    [Activity(Label = "TrenteArpents", Icon = "@mipmap/icon", Theme = "@style/SplashScreen", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "TrenteArpents", Icon = "@mipmap/ic_launcher", Theme = "@style/SplashScreen", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
